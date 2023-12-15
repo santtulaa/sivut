637264570b5a1f552423a17c15ba95dddbfb12ad
santtulaa <santtu_salmela@hotmail.com>
2023-12-15 21:22:22 +0200

    css and editing html

--- routes.py
+++ routes.py
@@ -1,31 +1,25 @@
-from flask import render_template, request, redirect, session, Response, Flask
+from flask import render_template, request, redirect, session, Flask, url_for
 from werkzeug.utils import secure_filename
-import os
+from base64 import b64encode
+from models import Img
 from app import app
+from db import db
 from users import get_user_id
 import users, reviews, fixes
 from reservations import reservation
 
-import reservations
-from PIL import Image
-import numpy as np
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
-from db import db_init, db
-from models import Img
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-from flask import render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
-from base64 import b64encode
-from models import Img  # Import your Img model
-##muutki pyt pitää importtaa jos on liikettä
-
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def index():
     list = reviews.get_list()
     return render_template("index.html", messages=list)
-
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -52,21 +46,20 @@ def register():
         session["user_name"] = name
 
         return redirect("/")
-    
-    
+
 @app.route("/login", methods=["GET", "POST"])
-def login():
+def login_page():
     if request.method == "GET":
         return render_template("login.html")
-    
+
     if request.method == "POST":
         name = request.form["name"]
         password = request.form["password"]
 
-    if not users.login(name, password):
-        return render_template("login.html")
-    return redirect("/")
-
+        if not users.login(name, password):
+            return render_template("login.html")
+        
+        return redirect("/")
 
 @app.route("/logout")
 def logout():
@@ -98,83 +91,25 @@ def sendfix():
         return redirect("/calendar")
     else:
         return render_template("error.html", message="Viestin lähetys ei onnistunut")
-    
+
 @app.route("/review")
 def review():
     return render_template("review.html")
 
 
-@app.route("/fix")
-def fix():
-    return render_template("fix.html")
 
 @app.route("/reserve", methods=["POST"])
 def reserve():
     todo = request.form.get("todo")
     date = request.form.get("date")
+    reservations=reservation()
     reservations.send(todo, date)
     return redirect(request.referrer)
-
-# @app.route('/upload', methods=['GET', 'POST'])
-# def home():
-#     if request.method == 'GET':
-#         return render_template('upload.html', msg='')
-
-    # image = request.files['file']
-    # img = Image.open(image)
-    # img = np.array(img)
-
-    # print(img)
-    # print(img.shape)
-
-    # return render_template('photos.html', msg='Your image has been uploaded')
-
-
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload():
-#     if request.method == 'GET':
-#         return render_template('upload.html')
-#     pic = request.files['pic']
-#     if not pic:
-#         return 'No pic uploaded!', 400
-
-#     filename = secure_filename(pic.filename)
-#     mimetype = pic.mimetype
-#     if not filename or not mimetype:
-#         return 'Bad upload!', 400
-
-#     img = Img(img=pic.read(), name=filename, mimetype=mimetype)
-#     db.session.add(img)
-#     db.session.commit()
-
-#     return 'Img Uploaded!', 200
-
-
-# @app.route('/<int:id>')
-# def get_img(id):
-#     img = Img.query.filter_by(id=id).first()
-#     if not img:
-#         return 'Img Not Found!', 404
-
-#     return Response(img.img, mimetype=img.mimetype)
-upload_folder = os.path.join("static", "uploads")
-
-app.config["UPLOAD"] = upload_folder
-
-import os
-
-UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'GET':
-        return render_template('upload.html')
+        return render_template('show_photos.html')
 
     pic = request.files['pic']
 
@@ -190,42 +125,14 @@ def upload():
         return redirect(url_for('show_photos'))
     else:
         return render_template('error.html', message='Invalid file type. Please upload a valid image file.')
-    
 
 @app.route('/show_photos')
 def show_photos():
-    # Retrieve images from the database
     images = Img.query.all()
 
-    # Pass b64encode to the template context
     template_context = {
         'images': images,
-        'b64encode': b64encode  # Pass b64encode to the template
+        'b64encode': b64encode  
     }
 
-    # Render the template and pass the list of images
     return render_template('show_photos.html', **template_context)
-
-# @app.route('/show_photos')
-# def show_photos():
-#     # Specify the path to your photo folder
-#     folder_path = os.path.join(app.static_folder, 'uploads')
-
-#     # List all image files in the folder (filter by extension)
-#     image_files = [f for f in os.listdir(folder_path) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
-
-#     # Render the template and pass the list of image files
-#     return render_template('show_photos.html', image_files=image_files)
-
-
-# @app.route("/image", methods=["GET", "POST"])
-# def image():
-#     if request.method == "GET":
-#         return render_template("image_render.html")
-#     if request.method == 'POST':
-#         file = request.files['img']
-#         filename = secure_filename(file.filename)
-#         file.save(os.path.join(app.config['UPLOAD'], filename))
-#         img = os.path.join(app.config['UPLOAD'], filename)
-#         return render_template('image_render.html', img=img)
-#     return render_template('image_render.html')
